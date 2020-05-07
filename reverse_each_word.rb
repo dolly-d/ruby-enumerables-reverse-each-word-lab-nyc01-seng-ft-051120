18d305a262ce452313633496fecb30732ab3099b
Dolly Desir <dolly.desir@gmail.com>
2020-05-07 21:23:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,10 @@
-def reverse_each_word(sentence1)
-  new_array = sentence1
+def reverse_each_word(sentence)
+  new_array = sentence
   return_array = []
-  new_array.each do|sentence1|
-    return_array << sentence1.reverse
+  new_array.each do|sentence|
+    return_array << sentence.reverse
   end
   return_array
 end
 
-def reverse_each_word(sentence2)
-  array = sentence2.split(" ") 
-  new_array = []
-  array.collect do|sentence|
-    new_array << sentence2.reverse
-  end
-  
 end
